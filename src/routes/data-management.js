260cb633770cf262e8806957ecb869f71c3f8c1a
TeamWan <52147478+copier1@users.noreply.github.com>
2022-09-06 02:31:13 +0800

    support windos

--- src/routes/data-management.js
+++ src/routes/data-management.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const DataManagementController = require("../controllers/data-management-controller");
 
-router.post("/populate", DataManagementController.populate);
+router.get("/populate", DataManagementController.populate);
 router.delete("/clear", DataManagementController.clear);
 
 module.exports = router;
